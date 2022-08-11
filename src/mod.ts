9c3b9bd21c15032c9136ba3e061e0fc4fce20f38
kawamataryo <ba068082@gmail.com>
2022-08-11 10:19:37 +0900

    [from now] 2022/08/11 10:19:37
    diff --git a/src/mod.ts b/src/mod.ts
    index 5ff84e4..7ecb1a9 100644
    --- a/src/mod.ts
    +++ b/src/mod.ts
    @@ -7,28 +7,32 @@ import { paint } from "./lib/painting.ts";
     import { DEFAULT_DIMENSIONS } from "./constants.ts";
    
     const html = await Deno.readTextFile("./src/source/index.html");
    -const css = await Deno.readTextFile("./src/source/style.css")
    +const css = await Deno.readTextFile("./src/source/style.css");
    
     const initialContainingBlock = {
    -  ...DEFAULT_DIMENSIONS , content: { x: 0, y: 0, width: 800, height: 800 }
    -}
    +  ...DEFAULT_DIMENSIONS,
    +  content: { x: 0, y: 0, width: 800, height: 800 },
    +};
    
     const rootNode = parseHtml(html);
     const stylesheet = parseCss(css);
     const styledNode = buildStyledTree(rootNode, stylesheet);
    -const layoutRoot = layoutTree(styledNode, JSON.parse(JSON.stringify(initialContainingBlock)));
    -const canvas = paint(layoutRoot, { ...initialContainingBlock.content} );
    +const layoutRoot = layoutTree(
    +  styledNode,
    +  JSON.parse(JSON.stringify(initialContainingBlock)),
    +);
    +const canvas = paint(layoutRoot, { ...initialContainingBlock.content });
    
     const base = createCanvas(canvas.width, canvas.height);
     const ctx = base.getContext("2d");
    
     [...Array(canvas.height).keys()].forEach((y) => {
       [...Array(canvas.width).keys()].forEach((x) => {
    -    const color = canvas.pixels[x + y * canvas.width]
    +    const color = canvas.pixels[x + y * canvas.width];
    
         ctx.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
         ctx.fillRect(x, y, 1, 1);
    -  })
    -})
    +  });
    +});
    
     await Deno.writeFile("image.png", base.toBuffer());

--- src/mod.ts
+++ src/mod.ts
@@ -7,28 +7,32 @@ import { paint } from "./lib/painting.ts";
 import { DEFAULT_DIMENSIONS } from "./constants.ts";
 
 const html = await Deno.readTextFile("./src/source/index.html");
-const css = await Deno.readTextFile("./src/source/style.css")
+const css = await Deno.readTextFile("./src/source/style.css");
 
 const initialContainingBlock = {
-  ...DEFAULT_DIMENSIONS , content: { x: 0, y: 0, width: 800, height: 800 }
-}
+  ...DEFAULT_DIMENSIONS,
+  content: { x: 0, y: 0, width: 800, height: 800 },
+};
 
 const rootNode = parseHtml(html);
 const stylesheet = parseCss(css);
 const styledNode = buildStyledTree(rootNode, stylesheet);
-const layoutRoot = layoutTree(styledNode, JSON.parse(JSON.stringify(initialContainingBlock)));
-const canvas = paint(layoutRoot, { ...initialContainingBlock.content} );
+const layoutRoot = layoutTree(
+  styledNode,
+  JSON.parse(JSON.stringify(initialContainingBlock)),
+);
+const canvas = paint(layoutRoot, { ...initialContainingBlock.content });
 
 const base = createCanvas(canvas.width, canvas.height);
 const ctx = base.getContext("2d");
 
 [...Array(canvas.height).keys()].forEach((y) => {
   [...Array(canvas.width).keys()].forEach((x) => {
-    const color = canvas.pixels[x + y * canvas.width]
+    const color = canvas.pixels[x + y * canvas.width];
 
     ctx.fillStyle = `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`;
     ctx.fillRect(x, y, 1, 1);
-  })
-})
+  });
+});
 
 await Deno.writeFile("image.png", base.toBuffer());
